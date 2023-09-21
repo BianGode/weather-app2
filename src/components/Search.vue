397d0ea75dd9cccd5410907514711fd5e894e5f0
BianGode <bian2205@gmail.com>
2023-09-21 13:45:34 +0200

    Added a todo file and location search on input bases

---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" v-model="query">
+        <h1>{{ query }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    props: ['query']
+}
+</script>
+
+<style scoped>
+input {
+    background-color: black;
+    color: white;
+    width: 200px;
+    height: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>This is the home view</h1>
+  <Search v-model="location" :query="{location}"/>
+  <button @click="getWeather">Get Weather</button>
+  <div v-if="weather !== 'niks'">
+    <h3>{{ weather.current.condition.text }}</h3>
+    <h3>{{ weather.current.feelslike_c }} Celsius</h3>
+    <h3>{{ weather.current.wind_kph }} Km/h</h3>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Search from "../components/Search.vue";
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+    Search
+  },
+  data() {
+    return {
+      weather: 'niks',
+      location: ''
+    }
+  },
+  methods: {
+    async getWeather() {
+      try {
+        // let response = await fetch('http://api.weatherapi.com/v1/current.json?key=b38dbcfab3d74cde9d0113841232009&q=' + this.location + '&aqi=no')
+        // this.weather = await response.json();
+        console.log(this.location)
+        // console.log(this.weather.current);
+        // console.log(this.weather.location);
+      } catch (err) {
+        console.log(err);
+      }
+    }
   }
 }
 </script>
